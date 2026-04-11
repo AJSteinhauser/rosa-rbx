d03493dfaf8ce5eaab680a57cc2a9c69570a660a
AJ Steinhauser <ajsteinhauser11@gmail.com>
2026-04-10 22:54:23 -0400

    Claude port to luau
    Claude port to luau

--- src/utils/helpers.ts
+++
@@ -1,9 +0,0 @@
-import { Position } from "../models/event.model"
-
-export const vector3ToPosition = (vector3: Vector3): Position => {
-  return {
-    x: vector3.X,
-    y: vector3.Y,
-    z: vector3.Z
-  }
-}
